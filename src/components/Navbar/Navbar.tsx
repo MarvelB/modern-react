5aff654425f08766357d8b6459a811e176ceaf8b
bryan.maravilla <bryan.maravilla@ticktradesystems.com>
2021-12-20 17:19:30 -0500

    commit for l 6

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+// import React from 'react';
+
+// interface NavBarPropType {
+//   name: string;
+// }
+
+const Navbar = () => (
+  <div className="navbar">
+    <h1>Blogs</h1> 
+    <div className="links">
+      <a href="/">Home</a>
+      <a href="/create">New Blog</a>
+    </div>
+  </div>
+);
+
+export default Navbar;
