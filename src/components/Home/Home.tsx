7948be55619ccf8e0e71320ee40e22f9c2872b36
bryan <bryan@devlift.io>
2021-12-20 22:08:49 -0500

    commit for l 7

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { SyntheticEvent } from "react";
 
-const Home = () => (
-  <div className="home">
-    <h2>Home Page</h2>
-  </div>
-);
+const Home = () => {
+
+  const handleClick = (event: SyntheticEvent) => {
+    console.log('perra', event)
+  }
+
+  const handleClickAgain = (name: string, event: SyntheticEvent) => {
+    console.log(`perra ${name}`, event)
+  }
+
+  return (
+    <div className="home">
+      <h2>Home Page</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain('maldita', e)}>Click me again</button>
+    </div>
+  )
+};
 
 export default Home;
