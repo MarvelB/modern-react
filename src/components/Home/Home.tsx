224c48977f657962c640e96cf58e53c6cb9b301b
bryan.maravilla <bryan.maravilla@ticktradesystems.com>
2021-12-21 11:08:30 -0500

    initial commit l10

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,11 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState<string>('Mario');
-  const [age, setAge] = useState<number>(25);
-
-  const handleClick = () => {
-    setName('Luigui');
-    setAge(30);
-  }
+  
 
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
     </div>
   )
 };
