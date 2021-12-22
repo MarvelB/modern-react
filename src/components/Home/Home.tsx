d52f927c8d6b61d6d9656538245ed039ac313506
bryan <bryan@devlift.io>
2021-12-21 21:52:30 -0500

    dl l19 commit

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,23 +5,34 @@ import BlogList from '../BlogList/BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
 
   // Empty array dependency, use effect runs only once
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-    .then(res => {
+    .then((res: Response) => {
+      if(!res.ok) {
+        throw Error('Could not fetch the data for that resource');
+      }
+
       return res.json();
     })
     .then((data: Blog[]) => {
       setBlogs(data);
       setIsLoading(false);
+      setError('');
+    })
+    .catch((err: TypeError) => {
+      setIsLoading(false);
+      setError(err.message);
     });
   }, []);
 
   return (
     <div className="home">
+      { error && <div>{ error }</div> }
       { isLoading && <div>Loading...</div>}
-      { !isLoading && <BlogList
+      { blogs && blogs.length > 0 && <BlogList
         blogs={blogs}
         title="All Blogs!" />}
     </div>
