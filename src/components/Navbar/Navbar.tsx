3fc43deed2bd291a634093a7afb14de14330022c
bryan <bryan@devlift.io>
2021-12-22 01:08:40 -0500

    dl l23 commit

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => (
   <div className="navbar">
     <h1>Blogs</h1> 
     <div className="links">
-      <a href="/">Home</a>
-      <a href="/create">New Blog</a>
+      <Link to="/">Home</Link>
+      <Link to="/create">New Blog</Link>
     </div>
   </div>
 );
